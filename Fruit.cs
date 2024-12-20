2fd34bca2599788e41e7d712a89abbd1ca42efee
2jaeyoung2 <i0579923@gmail.com>
2024-12-20 17:54:11 +0900

    [Feat]점수 별 열매 카운트

--- Fruit.cs
+++ Fruit.cs
@@ -12,7 +12,9 @@ namespace privateConsoleProject
         int randomItemPlace;
         float randomScore;
         int floorCount = 0;
-        
+        int[] itemCountPerScore = new int[5] { 0, 0, 0, 0, 0 };
+
+
         public void FloorCount(int distance, TileType[,] maze)
         {
             for (int i = 0; i < distance; i++)
@@ -27,7 +29,7 @@ namespace privateConsoleProject
             }
         }
 
-        public void MakeRandomFruit(int distance, TileType[,] maze)
+        public int[] MakeRandomFruit(int distance, TileType[,] maze)
         {
             for (int i = 0; i < distance; i++)
             {
@@ -42,10 +44,31 @@ namespace privateConsoleProject
                         {
                             maze[i, j].Type = (int)MazeCompo.item;
                             maze[i, j].Score = randomScore;
+                            if (maze[i, j].Score > 3)
+                            {
+                                itemCountPerScore[0]++;
+                            }
+                            else if (maze[i, j].Score > 3)
+                            {
+                                itemCountPerScore[1]++;
+                            }
+                            else if (maze[i, j].Score > 2.3)
+                            {
+                                itemCountPerScore[2]++;
+                            }
+                            else if (maze[i, j].Score > 1.7)
+                            {
+                                itemCountPerScore[3]++;
+                            }
+                            else
+                            {
+                                itemCountPerScore[4]++;
+                            }
                         }
                     }
                 }
             }
+            return itemCountPerScore;
         }
     }
 }
